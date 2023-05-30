46a1d941d51e368d3298b8b74e1a3c90870f36fe
Jessye Pereira <jessyekessia16@gmail.com>
2023-05-30 14:43:47 -0300

    Login e registro adicionado

--- app/sevidor-H-RES.py
+++ app/sevidor-H-RES.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # Importações
-from threading import *
+import threading
 import socket
+from threading import Lock
 # import hotel
 
 
@@ -10,63 +11,54 @@ HOST = 'localhost'
 PORT = 50000
 
 # Os semáforos
-lock_registro_semáforo = Lock()
+lock = Lock()
 
 #Função para processar as solicitações dos clientes
 def atender_clientes(socket_cliente, endereco_cliente, solicitacao):
     # Recebe a solicitação do cliente e decodifica
     solicitacao = solicitacao.recv(1024).decode().strip()
-    #Avisando que o cliente mandou mensagem
+        #Avisando que o cliente mandou mensagem
     print(f'Cliente mandou: {solicitacao}')
 
-    # processar a solicitação do cliente separando a tupla e tirando aquela parte desnecessária.
+        # processar a solicitação do cliente separando a tupla e tirando aquela parte desnecessária.
     solicitacao_partida = solicitacao.split()
-
-    # solicitação de registro    
-    if solicitacao_partida[0].upper() == 'REGISTRAR':
-        usuario = solicitacao_partida[1]
-        senha = solicitacao_partida[2]
-        
+    
     # Bloqueia o acesso ao elemento txt de registro que vai ser escrito agora
-        lock_registro_semáforo.acquire()
+    with lock:
+        # solicitação de registro    
+        if solicitacao_partida[0].upper() == 'REGISTRAR':
+            usuario = solicitacao_partida[1]
+            senha = solicitacao_partida[2]
 
-        # registro do usuário
-        # essa função precisa existir na classe hotel
-        if registro_usuario (usuario, senha):
-            return ('+OK 20 frase que lucas colocou')
-        else:
-            return ('-ERR 43 Registration failed')
-
-        # Libera o acesso ao recurso compartilhado
-        register_lock.release()
-
-    else:
-        response = "-ERR 40 regristo inválido"    
-                
-                
-                # Consulta a disponibilidade de quartos no banco de dados
-                
-                disponibilidade = consultar_disponibilidade()
-                resposta = f'Disponibilidade: {disponibilidade}'
-            elif solicitacao == 'reserva':
-                try:
-                # Processa a reserva do quarto no banco de dados
-                    resultado = processar_reserva()
-                    resposta = 'Reserva efetuada com sucesso!' 
-                except:
-                    resultado else 'Falha na reserva.'
-            elif solicitacao == 'cancelar':
-                # Cancela a reserva no banco de dados
-                resultado = cancelar_reserva()
-                resposta = 'Reserva cancelada com sucesso!' if resultado else 'Falha ao cancelar reserva.'
+            # registro do usuário
+            # essa função precisa existir na classe hotel. JOHNNER CRIE O HOTEL!
+            if hotel.registro_usuario(usuario, senha):
+                resposta = (str.encode('+OK 200 Usuário registrado com sucesso. \n'))
+                socket_cliente.send(resposta)
             else:
-                resposta = 'Solicitação inválida.'
+                resposta_erro = (str.encode(f'-ERR 403 Usuário já existe. \n'))
+                socket_cliente.send(resposta_erro)
 
-            socket_cliente.send(resposta.encode())
-            if not solicitacao:
-            # Se a solicitação estiver vazia, a conexão foi encerrada pelo cliente
-                print('Conexão encerrada pelo cliente:', endereco_cliente)
-                break
+        else:
+            resposta_erro = (str.encode(f'-ERR 430 Comando inválido. \n'))
+            socket_cliente.send(resposta_erro)
+             # libera o acesso ao recurso compartilhado
+
+        with lock:
+        # solicitação de login 
+            if solicitacao_partida[0].upper() == 'LOGIN':
+
+                # login do usuário
+                # essa função precisa existir na classe hotel. JOHNNER CRIE O HOTEL!
+                if hotel.login_usuario(usuario, senha):
+                        resposta = (str.encode('+OK 201 Usuário logado com sucesso. \n'))
+                        socket_cliente.send(resposta)
+                else:
+                    resposta = (str.encode('-ERR 403 Usuário não existe. \n'))
+                    socket_cliente.send(resposta)
+            else:
+
+
 
 
 
