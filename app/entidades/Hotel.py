640fe09e13713fd1d760f6f23a1f9c69860b92df
Lucas Andrade <lucasantos.dev@gmail.com>
2023-05-30 23:27:49 -0300

    Traduzindo AVL para portugues

--- app/entidades/Hotel.py
+++ app/entidades/Hotel.py
@@ -1,14 +1,10 @@
-from estruturas.avl import AVLTree
+from estruturas.avl import AVL
 
-# Instanciando AVL
-avl = AVLTree()
-
-# Criando a Classe Hotel
 class Hotel:
     def __init__(self):
-        self.__quartos_disponiveis = None # linkar com a estrutura AVL
-        self.__quartos_ocupados = None # linkar com a estrutura AVL
-        self.__clientes = [] 
+        self.__quartos_disponiveis = AVL()
+        self.__quartos_ocupados = AVL()
+        self.__clientes = []
 
     def reserva(self, id_quarto, checkin, chekout):
-        novareserva = Reserva(quarto, usuario, '12/03', '22/03')
+        pass
