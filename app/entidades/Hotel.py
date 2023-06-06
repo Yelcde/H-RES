ffe99c463c54c303deab1fe649bb52e0eee845d3
Lucas Andrade <lucasantos.dev@gmail.com>
2023-06-06 09:52:24 -0300

    Criando funcao para carregar dados dos quartos na AVL

--- app/entidades/Hotel.py
+++ app/entidades/Hotel.py
@@ -2,6 +2,7 @@ from comandos_usuario.login import login
 from comandos_usuario.registrar import registrar
 
 from entidades.Usuario import Usuario
+from entidades.Quarto import Quarto
 
 from estruturas.avl import AVL
 from estruturas.lista_encadeada import ListaEncadeada
@@ -11,10 +12,12 @@ class Hotel:
     Classe responsável por lidar com ações relativas ao hotel.
     '''
     def __init__(self):
-        self.__quartos_disponiveis = AVL()
+        self.__quartos = AVL()
         self.__quartos_ocupados = AVL()
         self.__clientes = ListaEncadeada()
+
         self.__carregar_usuarios()
+        self.__carregar_quartos()
 
     def registrar_cliente(self, login: str, senha: str) -> bool:
         '''
@@ -76,3 +79,25 @@ class Hotel:
             self.__clientes.append(usuario)
 
         arq_usuarios.close()
+
+    def __carregar_quartos(self):
+        '''
+        Método usado no momento que a classe é instanciada com o propósito de carregar os quartos salvos no arquivo "quartos.txt" na AVL do Hotel.
+        '''
+
+        arq_quartos = open('./app/quartos.txt')
+        quartos = arq_quartos.readlines()[1:] # remover cabeçalho do arquivo
+
+        for quarto in quartos:
+            quarto = quarto[:-1] # remove o \n do final
+            quarto = quarto.split(':')
+
+            numero = int(quarto[0])
+            tamanho = float(quarto[1])
+            disponivel = bool(quarto[2])
+            valor_diaria = float(quarto[3])
+
+            quarto = Quarto(numero, tamanho, disponivel, valor_diaria)
+            self.__quartos.inserir(quarto)
+
+        arq_quartos.close()
--- app/entidades/Quarto.py
+++ app/entidades/Quarto.py
@@ -2,60 +2,74 @@ class Quarto:
     def __init__(
         self,
         numero: int,
-        qtd_quartos: int = 2,
-        qtd_banheiros: int = 2,
         tamanho: float = 12,
-        preco_diaria: float = 30
+        disponivel: bool = True,
+        valor_diaria: float = 80,
+        qtd_quartos: int = 2,
+        qtd_banheiros: int = 2
     ):
         self.__numero = numero
+        self.__tamanho = tamanho
+        self.__disponivel = disponivel
+        self.__valor_diaria = valor_diaria
         self.__qtd_quartos = qtd_quartos
         self.__qtd_banheiros = qtd_banheiros
-        self.__tamanho = tamanho
-        self.__preco_diaria = preco_diaria
 
     @property
-    def numero (self) -> int:
+    def numero(self) -> int:
         '''
         Método para ter acesso ao número do quarto do hotel.
-        
         '''
         return self.__numero
 
     @property
-    def quantidade_quartos (self) -> int:
+    def quantidade_quartos(self) -> int:
         '''
         Método para ter acesso à quantidade de quartos no quarto.
-        
+
         '''
         return self.__qtd_quartos
 
     @property
-    def quantidade_banheiros (self) -> int:
+    def quantidade_banheiros(self) -> int:
         '''
         Método para ter acesso à quantidade de banheiros no quarto.
-        
+
         '''
         return self.__qtd_banheiros
-    
+
     @property
-    def tamanho (self) -> int:
+    def disponivel(self) -> bool:
         '''
-        Método para ter acesso ao tamanho geral do quarto.
-        
+        Método para ter acesso se o quarto está disponível.
         '''
-        return self.__tamanho
-    
+        return self.__disponivel
+
     @property
-    def preco_diaria (self) -> int:
+    def tamanho(self) -> str:
+        '''
+        Método para ter acesso ao tamanho do quarto em metro quadrado(m2).
+        '''
+        return f'{self.__tamanho}m2'
+
+    @property
+    def valor_diaria(self) -> str:
         '''
         Método para ter acesso ao preço do quarto por diária.
-        
         '''
-        return self.__preco_diaria
-    
+        return f'R${self.__valor_diaria:.2f}'
+
     def __str__(self) -> str:
         '''
         Método para imprimir as informações do quarto para um cliente.
-
         '''
-        return f"Número: {self.__numero} | Qtd. de quartos: {self.__qtd_quartos} | Qtd. de banheiros: {self.__qtd_banheiros} | Tamanho: {self.__tamanho} | Preço diária: {self.__preco_diaria}"
+        return f"Número: {self.__numero} | Disponível: {self.__disponivel} | Qtd. de quartos: {self.__qtd_quartos} | Qtd. de banheiros: {self.__qtd_banheiros} | Tamanho: {self.__tamanho} | Preço diária: {self.__valor_diaria}"
+
+    def __eq__(self, numero: int) -> bool:
+        return self.__numero == numero
+
+    def __lt__(self, numero: int) -> bool:
+        return self.__numero < numero
+
+    def __gt__(self, numero: int) -> bool:
+        return self.__numero > numero
