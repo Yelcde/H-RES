73fbc8b375ef53ef1c62e06e69e08bd84ef06016
Lucas Andrade <lucasantos.dev@gmail.com>
2023-06-28 22:10:04 -0300

    Atualizando repositorio de clientes

--- app/entidades/Repositorio_Clientes.py
+++ app/entidades/Repositorio_Clientes.py
@@ -1,19 +1,24 @@
 from estruturas.lista_encadeada import ListaEncadeada
-
+from entidades.Usuario import Usuario
 
 class Repositorio_Clientes():
-    def __init__(self) -> None:
+    def __init__(self):
         self.__clientes = ListaEncadeada()
 
         self.__carregar_clientes() # Carrega os dados do arquivo "usuarios.txt"
 
-
-    def salvar_cliente(self, cliente):
+    def salvar(self, cliente: Usuario):
         self.__clientes.append(cliente)
+
+    def buscar_por_nome(self, nome: str) -> int:
+        return self.__clientes.busca(nome)
+
+    def buscar_por_posicao(self, posicao: int) -> Usuario:
+        return self.__clientes.elemento(posicao)
 
     def __carregar_clientes(self):
         '''
-        Método usado no momento que a classe é instanciada com o propósito de carregar os usuário salvos no arquivo "usuarios.txt" na lista encadeada do Hotel.
+        Método usado no momento que a classe é instanciada com o propósito de carregar os usuário salvos no arquivo "usuarios.txt" na lista encadeada de clientes.
         '''
         arq_usuarios = open('./app/usuarios.txt')
 
